e9222cc54e9e2c5b1470a4ff243053196a179168
Shubham <shubham.lynux@gmail.com>
2020-12-13 23:04:09 +0530

    initial commit

---
+++ src/main/java/Subarray_with_given_sum.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Subarray_with_given_sum {
+
+    static void findSubArray(int[] arr, int S){
+        int prefixSum = 0, startIndex = 0, endIndex = 0;
+        for(int i =0; i < arr.length; i++){
+            prefixSum += arr[i];
+            endIndex = i;
+            while(prefixSum > S) {
+                prefixSum -= arr[startIndex];
+                startIndex++;
+
+            }
+            if(prefixSum == S) break;
+        }
+        for(int x = startIndex; x <= endIndex; x++){
+            System.out.print(arr[x]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner reader = new Scanner(System.in);
+        int testCaseCount = reader.nextInt();
+        while(testCaseCount > 0){
+            int arraySize = reader.nextInt();
+            int S = reader.nextInt();
+            int[] arr = new int[arraySize];
+            for(int x = 0; x < arraySize; x++){
+                arr[x] = reader.nextInt();
+            }
+            findSubArray(arr,S);
+            testCaseCount--;
+        }
+
+
+    }
+
+}
